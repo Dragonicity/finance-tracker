ff6160c041fc35aed4f57594f94dda5d838a020a
Mark-Nelson <mark.nelson@dragonicity.com>
2016-07-18 16:41:40 +0800

    Add destroy and show actions

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,12 @@
+class FriendshipsController < ApplicationController
+
+  def destroy
+    @friendship = current_user.friendships.where(friend_id: params[:id].first)
+    @friendship.destroy_all
+    respond_to do |format|
+      format.html { redirect_to my_friends_path, 
+        notice: "Friends was successfully removed." }
+    end
+  end
+
+end
